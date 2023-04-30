2f724cc80a159e04410105b42489e2016ee0d501
Morrrun <mifon.199708@gmail.com>
2023-04-30 16:50:52 +0400

    Setter Injection

--- src/main/java/org/alexsandrov/spring/database/pool/ConnectionPool.java
+++ src/main/java/org/alexsandrov/spring/database/pool/ConnectionPool.java
@@ -5,16 +5,19 @@ import java.util.Map;
 
 
 public class ConnectionPool {
+    private final String username;
+
+    private final Integer poolSize;
+    private final List<Object> args;
+    private Map<String, Object> properties;
     public ConnectionPool(String username, Integer poolSize,
-                          List<Object> args, Map<String, Object> properties) {
+                          List<Object> args) {
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
-        this.properties = properties;
     }
 
-    private final String username;
-    private final Integer poolSize;
-    private final List<Object> args;
-    private final Map<String, Object> properties;
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
 }
